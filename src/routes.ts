77ac2451403d94c64e4a03be95514cdbb29f94d4
Ayeye11 <ayeye.devp@gmail.com>
2025-04-16 17:47:55 -0300

    refactor: delete unused files

--- src/routes.ts
+++ src/routes.ts
@@ -1,48 +1,33 @@
 import { RequestImpl, ResponseImpl, Router } from "@infrastructure/http";
 import type { IncomingMessage, ServerResponse } from "node:http";
 import type { ControllerModule, MiddlewareModule } from "./modules";
-import { Action, Category } from "@domain/security/permissions";
 
 export type Routes = (req: IncomingMessage, res: ServerResponse) => void;
 
 const setupRoutes = (
-	middlewares: MiddlewareModule,
-	controllers: ControllerModule,
+  middlewares: MiddlewareModule,
+  controllers: ControllerModule,
 ): Routes => {
-	return (sReq, sRes) => {
-		const req = new RequestImpl(sReq);
-		const res = new ResponseImpl(sRes);
+  return (sReq, sRes) => {
+    const req = new RequestImpl(sReq);
+    const res = new ResponseImpl(sRes);
 
-		// Main
-		const api = new Router(req, res, "/api");
+    // Main
+    const api = new Router(req, res, "/api");
 
-		// ---- AUTH
-		const auth = api.subRouter("/auth");
-		const authControllers = controllers.getAuth();
-		auth.post("/register", authControllers.register.bind(authControllers));
-		auth.post("/login", authControllers.login.bind(authControllers));
+    // ---- AUTH
+    const auth = api.subRouter("/auth");
+    const authControllers = controllers.getAuth();
+    auth.post("/register", authControllers.register.bind(authControllers));
+    auth.post("/login", authControllers.login.bind(authControllers));
 
-		// ---- PRODUCTS
-		const authMiddlewares = middlewares.getAuth();
-		const products = api.subRouter("/products", authMiddlewares.authRequired());
-		products.get("/test", (_req, res) => {
-			res.sendSuccess(200, "all ok in test");
-		});
-		products.get(
-			"/test2",
-			authMiddlewares.permRequired(Category.PRODUCTS, Action.VIEW),
-			(_req, res) => {
-				res.sendSuccess(200, "all ok in test2");
-			},
-		);
+    // Run Routes
+    const handled = api.handleRequest();
 
-		// Run Routes
-		const handled = api.handleRequest();
-
-		if (!handled) {
-			res.sendError(404, "Not found");
-		}
-	};
+    if (!handled) {
+      res.sendError(404, "Not found");
+    }
+  };
 };
 
 export default setupRoutes;
